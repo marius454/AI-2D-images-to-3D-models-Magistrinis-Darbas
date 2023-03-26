7e24254d1917df75b842a42c9b2fe674d8c71af8
marius454 <marius.bieliauskas@mif.stud.vu.lt>
2023-03-26 18:59:05 +0300

    sync

--- ThreeD_gan/threeD_gan.py
+++ ThreeD_gan/threeD_gan.py
@@ -1,16 +1,32 @@
 import numpy as np
 import tensorflow as tf
-import variables as var
-import data_processing as dp
 import dataIO as d
 import custom_layers
 import os
 import time
 import gc
 
+
+from .. import variables as var
+from .. import data_processing as dp
+import threeD_gan_blocks as blocks
+
 from tensorflow_probability.python.distributions import kullback_leibler as kl_lib
 import tensorflow_probability as tfp
 
-class threeD_gan(tf.keras.Model):
+class Generator(tf.keras.Layer):
     def __init__(self):
-        super(threeD_gan, self).__init__()
+        super(Generator, self).__init__()
+        self.block_1 = blocks.Conv3DTransposeBlock()
+
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+class Encoder(tf.keras.Model):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+class ThreeD_gan(tf.keras.Model):
+    def __init__(self):
+        super(ThreeD_gan, self).__init__()
--- ThreeD_gan/threeD_gan_blocks.py
+++ ThreeD_gan/threeD_gan_blocks.py
@@ -9,19 +9,32 @@ import time
 import gc
 
 class Conv3DTransposeBlock(tf.keras.layers.Layer):
-    def __init__(self, input_shape, filter, strides, padding='same', use_batch_norm=True, use_ReLU=True):
+    def __init__(self, input_shape, filter, strides, padding='same'):
         super(Conv3DTransposeBlock, self).__init__()
         self.conv_layer = tf.keras.layers.Conv3DTranspose(filter, kernel_size=(4, 4, 4), 
             strides=strides, use_bias=False, input_shape=input_shape, padding=padding,
             kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02))
-        if use_batch_norm:
-            self.batch_norm_layer = tf.keras.layers.BatchNormalization()
-        if use_ReLU:
-            self.ReLU_layer = tf.keras.layers.ReLU()
+        self.batch_norm_layer = tf.keras.layers.BatchNormalization()
+        self.ReLU_layer = tf.keras.layers.ReLU()
 
-    def call(self, inputs):
-        x = self.conv_layer(inputs)
-        x = tf.nn.relu(x)
-        x = self.linear_2(x)
-        x = tf.nn.relu(x)
-        return self.linear_3(x)
+    def call(self, input, use_batch_norm=True, use_ReLU=True):
+        x = self.conv_layer(input)
+        if use_batch_norm:
+            x = self.batch_norm_layer(x)
+        if use_ReLU:
+            x = self.ReLU_layer(x)
+        return x
+
+
+
+# def Conv3DTransposeBlock(self, input, input_shape, filter, strides, padding='same', use_batch_norm=True, use_ReLU=True):
+#     x = tf.keras.layers.Conv3DTranspose(filter, kernel_size=(4, 4, 4), 
+#         strides=strides, use_bias=False, input_shape=input_shape, padding=padding,
+#         kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02))(input)
+
+#     if use_batch_norm:
+#         x = tf.keras.layers.BatchNormalization()(x)
+#     if use_ReLU:
+#         x = tf.keras.layers.ReLU()(x)
+
+#     return x
